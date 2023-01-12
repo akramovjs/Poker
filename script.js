a08607e9f94de75c7d09afa32c42c813b9b282ba
muhsindev0 <muhsinjonmullajonov0@gmail.com>
2023-01-13 01:22:53 +0500

    assa

--- script.js
+++ script.js
@@ -28,9 +28,38 @@ const animatsiya = () =>{
 
 animatsiya();
 
-var loader = document.querySelector(".big");
-window.addEventListener("DOMContentLoaded", function df() {
-    setTimeout(() => {
-        loader.style.display = "none";
-    }, 4000);
-})
+// var loader = document.querySelector(".big");
+// window.addEventListener("DOMContentLoaded", function df() {
+    //     setTimeout(() => {
+        //         loader.style.display = "none";
+        //     }, 4000);
+        // })
+        
+        
+        
+        
+        // const allLiTags = document.querySelectorAll(".nav-links_a li");
+        // function playingSong() {
+        //     for(let j = 0; j < 7; j++){
+        //         console.log(j);
+        //         //  let audioTag = allLiTags[j].querySelector(".audio-duration");
+        //         //
+        //         if(allLiTags[j].classList.contains("activex")){
+        //             allLiTags[j].classList.remove("activex");
+        //             // alert(1)
+        //             // let's get that audio duration value and pass to .audio-duration innerText
+        //         }
+                
+        //         // if there is an li tag which li index is equal to musicIndex
+        //         // then this music is playing now and we'll style it
+                
+        //         //  if(allLiTags[j].getAttribute("li-index") == musicIndex){
+        //             //      allLiTags[j].classList.add("activex");
+        //             //     //  audioTag.innerText="playing"
+        //             //  }
+                    
+        //             // adding on click attribute in all li tags
+        //             allLiTags[j].setAttribute("onclick", "clicked(this)")
+        //         }
+        //     }
+        //     playingSong()
