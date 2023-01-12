8e16235d95c2ef2d556f69a0cb5850a257af74fc
muhsindev0 <muhsinjonmullajonov0@gmail.com>
2023-01-12 16:07:00 +0500

    manimcha tugri buldi endi

---
+++ script.js
@@ -0,0 +1,29 @@
+const animatsiya = () =>{
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    const ext = document.querySelector(".ext")
+
+
+    // listeners
+
+    burger.addEventListener("click", () =>{
+        nav.classList.toggle("nav-active");
+        
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation="";
+            }else{
+              link.style.animation = `move 0.5s ease forwards ${index / 5 + 0.35}s`; 
+            }
+        });
+
+        //Animated burger
+        burger.classList.toggle("toggle");
+    })
+    ext.addEventListener("click", ()=>{
+        nav.classList.remove("nav-active")
+    })
+}
+
+animatsiya();
